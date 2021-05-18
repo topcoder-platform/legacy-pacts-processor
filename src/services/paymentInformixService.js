43c9e0d9f9d34edc4b07ccde865fae894e30274b
James Cori <jcori@topcoder.com>
2021-05-18 09:51:24 -0400

    Cleaning up query to simplify logging

--- src/services/paymentInformixService.js
+++ src/services/paymentInformixService.js
@@ -8,50 +8,37 @@ const {
 } = require('config')
 const { executeQueryAsync } = require('../util/informixWrapper')
 
+/**
+ Removed Columns
+ pd.algorithm_round_id,
+    pd.component_project_id,
+    pd.cockpit_project_id,
+    pd.algorithm_problem_id,
+    pd.studio_contest_id,
+    pd.component_contest_id,
+    pd.digital_run_stage_id,
+    pd.digital_run_season_id,
+    pd.parent_payment_id,
+    pd.total_amount,
+    pd.installment_number,
+    pd.digital_run_track_id,
+    u.first_name,
+    u.middle_name,
+    u.last_name,
+ */
 const QUERY_GET_PAYMENTS = `SELECT 
-p.payment_id, 
-pd.payment_detail_id,
-pd.payment_desc, 
-pd.payment_type_id, 
-pd.payment_method_id, 
-p.create_date, 
-pd.create_date as modify_date,
-pt.payment_type_desc, 
-pm.payment_method_desc, 
-pd.net_amount, 
-pd.payment_status_id, 
-s.payment_status_desc,
-p.user_id, 
-u.handle, 
-u.first_name, 
-u.middle_name, 
-u.last_name,
-pd.date_modified, 
-pd.date_paid, 
-pd.gross_amount, 
-nvl(pdsrx.payment_status_reason_id, 0) as payment_status_reason_id,
-psrl.payment_status_reason_desc,
-pd.algorithm_round_id, 
-pd.component_project_id, 
-pd.cockpit_project_id, 
-pd.algorithm_problem_id,
-pd.studio_contest_id, 
-pd.component_contest_id, 
-pd.digital_run_stage_id,
-pd.digital_run_season_id, 
-pd.parent_payment_id, 
-pd.total_amount, 
-pd.installment_number, 
-pd.digital_run_track_id, 
-pd.jira_issue_id
+p.payment_id, pd.jira_issue_id, pd.payment_detail_id, pd.payment_desc, pd.payment_type_id, pd.payment_method_id, p.create_date, 
+pd.create_date as modify_date, pt.payment_type_desc, pm.payment_method_desc, pd.net_amount, pd.payment_status_id, s.payment_status_desc,
+p.user_id, u.handle, pd.date_modified, pd.date_paid, pd.gross_amount, nvl(pdsrx.payment_status_reason_id, 0) as payment_status_reason_id,
+psrl.payment_status_reason_desc 
 FROM payment p
-INNER JOIN payment_detail pd ON pd.payment_detail_id = p.most_recent_detail_id
-INNER JOIN payment_type_lu pt ON pt.payment_type_id = pd.payment_type_id
-INNER JOIN payment_method_lu pm ON pm.payment_method_id = pd.payment_method_id
-INNER JOIN payment_status_lu s ON s.payment_status_id = pd.payment_status_id
-INNER JOIN user u ON u.user_id = p.user_id
-LEFT OUTER JOIN payment_detail_status_reason_xref pdsrx ON pdsrx.payment_detail_id = pd.payment_detail_id
-LEFT OUTER JOIN payment_status_reason_lu psrl ON psrl.payment_status_reason_id= pdsrx.payment_status_reason_id
+  INNER JOIN payment_detail pd ON pd.payment_detail_id = p.most_recent_detail_id
+  INNER JOIN payment_type_lu pt ON pt.payment_type_id = pd.payment_type_id
+  INNER JOIN payment_method_lu pm ON pm.payment_method_id = pd.payment_method_id
+  INNER JOIN payment_status_lu s ON s.payment_status_id = pd.payment_status_id
+  INNER JOIN user u ON u.user_id = p.user_id
+  LEFT OUTER JOIN payment_detail_status_reason_xref pdsrx ON pdsrx.payment_detail_id = pd.payment_detail_id
+  LEFT OUTER JOIN payment_status_reason_lu psrl ON psrl.payment_status_reason_id= pdsrx.payment_status_reason_id
 WHERE pdsrx.payment_status_reason_id = ${PAYMENT_STATUS_REASONS.V5_PAYMENT_CREATION}
   AND pd.payment_status_id = ${PAYMENT_STATUSES.ON_HOLD}`
 
